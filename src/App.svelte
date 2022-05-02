30b1a024a20170466814625ba17c22de3b5f9bd3
David E Luna M <davidlunamontilla@gmail.com>
2022-05-02 06:54:25 -0500

    Se le agregan los créditos

--- src/App.svelte
+++ src/App.svelte
@@ -7,9 +7,23 @@
      * @type { Promise<Object<string, any>> }
      */
     // let data = getData("api/");
-    let data = getData("https://graditest-store.myshopify.com/products/free-trainer-3-mmw.js");
+    let data = getData(
+        "https://graditest-store.myshopify.com/products/free-trainer-3-mmw.js"
+    );
 </script>
 
 <main id="app" class="main">
     <Products {data} />
 </main>
+
+<footer class="footer">
+    <div class="attribution">
+        A technical test by <a href="https://www.gradiweb.com/" target="_blank"
+            >Gradiweb.</a
+        >
+        Coded by
+        <a href="https://github.com/dlunamontilla/" target="_blank"
+            >David E Luna M.</a
+        >
+    </div>
+</footer>
