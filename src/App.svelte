87c330ca3188822e958fc59e3127a122568e39e2
David E Luna M <davidlunamontilla@gmail.com>
2022-05-02 06:43:39 -0500

    Se actualiza la ruta de la API

--- src/App.svelte
+++ src/App.svelte
@@ -6,8 +6,8 @@
      * Datos de la API de productos
      * @type { Promise<Object<string, any>> }
      */
-    let data = getData("api/");
-    // let data = getData("https://graditest-store.myshopify.com/products/free-trainer-3-mmw.js");
+    // let data = getData("api/");
+    let data = getData("https://graditest-store.myshopify.com/products/free-trainer-3-mmw.js");
 </script>
 
 <main id="app" class="main">
